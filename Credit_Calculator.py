18817c8959596ea35ea275475ec749d03c7068e1
MajewskiL <66209640+MajewskiL@users.noreply.github.com>
2020-06-02 12:12:34 +0200

    Update Credit_Calculator.py

--- Credit_Calculator.py
+++ Credit_Calculator.py
@@ -35,7 +35,6 @@ if arg["payment"] != "": payment = int(arg["payment"])
 if arg["periods"] != "": periods = int(arg["periods"])
 if arg["interest"] != "": interest = float(arg["interest"])/1200
 
-#print(principal,payment,periods,interest)
 # sprawdzanie kompletności parametrów
 if type not in ("diff","annuity"): bug = True
 if type == "diff" and arg["payment"] != "" : bug = True
@@ -57,14 +56,16 @@ else:
         if principal == 0:
             principal = credit_principal(payment,interest,periods)
             credit = annuity_payment(principal,interest,periods)
-            print(math.ceil((credit * periods) - principal), math.trunc(principal))
+            print(f"Overpayment = {math.ceil((credit * periods) - principal)}")
+            print(f"Your credit principal = {math.trunc(principal)}!")
         elif payment == 0:
             credit = annuity_payment(principal,interest,periods)
             principal = credit_principal(credit,interest,periods)
-            print(math.trunc((math.ceil(credit) * periods) - principal), math.ceil(credit))
+            print(f"Overpayment = {math.trunc((math.ceil(credit) * periods) - principal)}")
+            print(f"Your annuity payment = {math.ceil(credit)}!")
         elif periods == 0:
             periods = years(payment,principal,interest)
-            print(math.ceil(periods / 12))
+            print(f"You need {math.ceil(periods / 12)} years to repay this credit!")
             print(f"Overpayment = {(payment * math.ceil(periods)) - principal}")
     else:
         credit = 0
@@ -73,4 +74,3 @@ else:
             credit += single_month
             print(f"Month {m}: paid out {single_month}")
         print(f"Overpayment = {credit - principal}")
-Credit_
